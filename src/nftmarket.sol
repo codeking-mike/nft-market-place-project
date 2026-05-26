41a9a4e1f0aebe3b165af8d020461ec6f3b027b7
Michael Idam <princemicare@gmail.com>
2026-05-26 14:10:40 +0100

    added IERC721

--- src/nftmarket.sol
+++ src/nftmarket.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.33;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {IERC712} from "@openzeppelin/contract/token/IERC721/IERC721.sol";
 
 contract nftmarket is ERC721 {
     
